3219ef935092c626fef5e8be14bdfda6a494433c
Gundars <gundar.geida@gmail.com>
2023-09-14 16:54:14 +0100

    done 2

--- get-interests.js
+++ get-interests.js
@@ -1,25 +1,58 @@
 const fs = require("fs");
-//import { readFile } from "node:fs";
 const https = require("https");
-let least = [];
-fs.readFile("./northcoders.json", "utf-8", (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    least = JSON.parse(data);
+
+fs.readFile("./northcoders.json", "utf8", (err, data) => {
+  if (err) console.log("error");
+  else {
+    const objData = JSON.parse(data);
+    const limit = objData.length;
+    const userInterests = [];
+
+    let counter = [0];
+    objData.forEach((person) => {
+      getUrl(
+        "nc-leaks.herokuapp.com",
+        `/api/people/${person.username}/interests`,
+        "interests.json",
+        limit,
+        counter,
+        userInterests
+      );
+    });
   }
-  console.log(least);
 });
-function getInterests() {
+
+function getUrl(url, path, fileName, limit, counter, userInterests) {
   const options = {
-    hostname: "nc-leaks.herokuapp.com",
-    path: "/api/people/:username/interests",
+    hostname: `${url}`,
+    path: `${path}`,
     method: "GET",
   };
-  const request = 
-  //const usernames = least.map((person) => {
-  // person[username];
-  //});
+  const request = https.request(options, (response) => {
+    let content = "";
+    response.on("data", (packet) => {
+      content += packet.toString();
+    });
+    response.on("end", () => {
+      const passedContent = JSON.parse(content);
+      userInterests.push(passedContent.person);
+      counter[0]++;
+
+      if (counter[0] === limit) {
+        console.log(userInterests);
+        fs.writeFile(
+          `${fileName}`,
+          JSON.stringify(userInterests),
+          (err, result) => {
+            if (err) console.log(err);
+            else {
+              console.log("interests.json built successfully");
+            }
+          }
+        );
+      }
+    });
+  });
+  request.end();
 }
 
-getInterests();
